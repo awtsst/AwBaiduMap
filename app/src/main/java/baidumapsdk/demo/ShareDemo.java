bdb9af3ef3fae2836f186cb70d87a5c5db958f78
sunshuntao <staryumou@163.com>
2016-04-05 17:39:36 +0800

    The main function is to BaiduMap function ; i use gradle build to complile my project ,and success ,in the app/buil you can see apk ;This is in the ; and there are some suggest for you , you can see the websit : my.oschina.net/aibenben/blog/371889

---
+++ app/src/main/java/baidumapsdk/demo/ShareDemo.java
@@ -0,0 +1,25 @@
+package baidumapsdk.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.wtsst.hp.awbaidumap.R;
+
+public class ShareDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_demo);
+    }
+
+    public void startShareDemo(View view) {
+        Intent intent = new Intent();
+        intent.setClass(this, ShareDemoActivity.class);
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/wtsst/hp/awbaidumap/MainActivity.java
@@ -0,0 +1,47 @@
+package com.wtsst.hp.awbaidumap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import baidumapsdk.demo.BMapApiDemoMain;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView bmapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext；
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        bmapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bmapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bmapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        bmapView.onPause();
+    }
+    public void allClick(View view){
+        Intent intent = new Intent(MainActivity.this,BMapApiDemoMain.class);
+        startActivity(intent);
+
+    }
+}
